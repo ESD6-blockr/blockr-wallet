ed467e1fabe61b2fdec2da2d342e95b41039019b
Roel van den Boom <roelvandenboom@inogo.nl>
2019-05-28 13:57:59 +0200

    Add toast

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Route, Router, Switch } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Grid, Segment } from "semantic-ui-react";
 import "../global.scss";
 import routerHistory from "../store/routerHistory";
@@ -37,6 +39,7 @@ const Application = () => (
                 </Segment>
             </Grid.Column>
         </Grid>
+        <ToastContainer />
         <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import { toast } from "react-toastify";
 import { IRootState } from "reducers";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 import { login } from "../../actions/authentication.actions";
@@ -36,20 +37,21 @@ class Login extends React.Component<Props, DefaultState> {
         this.setState({
             publicKey: element.target.value,
         });
-    }
+    };
 
     public handlePrivateKeyChange = (element) => {
         this.setState({
             privateKey: element.target.value,
         });
-    }
+    };
 
     public login = () => {
         this.props.login(this.state.publicKey, this.state.privateKey);
-    }
+    };
 
     public componentDidUpdate() {
         if (this.props.currentUser) {
+            toast.success("Logged in!");
             goToUrl("/profile");
         }
     }
