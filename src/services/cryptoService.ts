99def40ac650565b493d420552bd8902ed536f40
MIKE <maikkeyy@gmail.com>
2019-06-15 17:45:02 +0200

    Added variable password length for encrypting/decrypting

--- src/services/cryptoService.ts
+++ src/services/cryptoService.ts
@@ -1,21 +1,26 @@
 import crypto from "crypto";
+import pbkdf2 from "pbkdf2";
 const algorithm = "aes-256-cbc";
 
 // https://gist.github.com/vlucas/2bd40f62d20c1d49237a109d491974eb
 export class CryptoService {
-    public encrypt = (text, key) => {
+    public encrypt = (text, password) => {
+        const derivedKey = pbkdf2.pbkdf2Sync(password, "salt", 1, 32, "sha512");
+
         const iv = crypto.randomBytes(16);
-        const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+        const cipher = crypto.createCipheriv(algorithm, Buffer.from(derivedKey), iv);
         let encrypted = cipher.update(text);
         encrypted = Buffer.concat([encrypted, cipher.final()]);
         return iv.toString("hex") + ":" + encrypted.toString("hex");
     };
 
-    public decrypt = (text, key) => {
+    public decrypt = (text, password) => {
+        const derivedKey = pbkdf2.pbkdf2Sync(password, "salt", 1, 32, "sha512");
+
         const textParts = text.split(":");
         const iv = new Buffer(textParts.shift(), "hex");
         const encryptedText = new Buffer(textParts.join(":"), "hex");
-        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(derivedKey), iv);
         let decrypted = decipher.update(encryptedText);
         decrypted = Buffer.concat([decrypted, decipher.final()]);
         return decrypted.toString();
